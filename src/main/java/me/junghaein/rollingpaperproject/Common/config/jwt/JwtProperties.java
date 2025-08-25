34c8eb405a8bbaa10c8119772c082362fac6a8e4
haeinjung3 <haeinj1215@gmail.com>
2025-08-26 05:11:02 +0900

    2025-08-26 중간과정

--- src/main/java/me/junghaein/rollingpaperproject/Common/config/jwt/JwtProperties.java
+++ src/main/java/me/junghaein/rollingpaperproject/Common/config/jwt/JwtProperties.java
@@ -12,5 +12,5 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties("jwt")
 public class JwtProperties {
     private String issuer;
-    private String secretKey;
+    private String secretKey = "test";
 }
